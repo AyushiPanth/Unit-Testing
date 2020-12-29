7c075f8d0970dfe31cb1cd704f1934177f77e86d
Ayushi Panth <54657980+AyushiPanth@users.noreply.github.com>
2020-12-29 13:23:59 +0530

    Solved errors

--- C++/range.cpp
+++ C++/range.cpp
@@ -1,6 +1,7 @@
 // Range => [low, high)
 
 #include <iostream>
+#include <string>
 #include <vector>
 #include <algorithm>
 using namespace std;
@@ -12,108 +13,108 @@ class Range {
 	
 	public:
 	    Range() {
-	    	this.low = 0;
-		    this.high = 0;
+	    	this->low = 0;
+		    this->high = 0;
 	    }
 
 	    Range(int end) {
-		    this.low = 0;
-		    this.end = end;
+		    this->low = 0;
+		    this->high = end;
 	    }
 
 	    Range(int first, int second) {
-	    	this.low = min(first, second);
-		    this.high = max(first, second) - 1;
+	    	this->low = min(first, second);
+		    this->high = max(first, second) - 1;
 	    }
 
 	    void reset() {
-		    this.low = 0;
-		    this.high = 0;
+		    this->low = 0;
+		    this->high = 0;
     	}
 
 	    void rightStretch() {
-		    this.high++;
+		    this->high++;
 	    }
 
 	    void rightStretch(int size) {
-		    this.high += size;
+		    this->high += size;
     	}
 
     	void leftStretch() {
-	    	this.low--;
+	    	this->low--;
     	}
 
     	void leftStretch(int size) {
-	    	this.low -= size;
+	    	this->low -= size;
 	    }
 
 	    void stretch() {
-    		this.low--;
-    		this.high++;
+    		this->low--;
+    		this->high++;
     	}
 
     	void stretch(int size) {
-    		this.low -= size;
-    		this.high += size;
+    		this->low -= size;
+    		this->high += size;
     	}
 
     	void squeeze() {
-		    this.low++;
-		    this.high--;
-		    if(this.low > this.high)
-		    	this.reset();
+		    this->low++;
+		    this->high--;
+		    if(this->low > this->high)
+		    	this->reset();
     	}
 
     	void squeeze(int size) {
-    		this.low += size;
-    		this.high -= size;
-    		if(this.low > this.high)
-    			this.reset();
+    		this->low += size;
+    		this->high -= size;
+    		if(this->low > this->high)
+    			this->reset();
     	}
     	
     	void shift(int shifter) {
-    		this.low += shifter;
-    		this.high += shifter;
+    		this->low += shifter;
+    		this->high += shifter;
     	}
     
     	int length() {
-    		return this.high - this.low + 1;
+    		return this->high - this->low + 1;
     	}
     	
     	string toString() {
-    		return "[" + toString(this.low) + "," + toString(this.high + 1) + ")";
+    		return "[" + to_string(this->low) + "," + to_string(this->high + 1) + ")";
     	}
     
     	bool contains(int element) {
-    		return this.low <= element && element <= this.high;
+    		return this->low <= element && element <= this->high;
     	}
     
     	bool contains(Range r) {
-    		return this.low <= r.low && this.high >= r.high;
+    		return this->low <= r.low && this->high >= r.high;
     	}
     
     	bool equals(Range r) {
-    		return r.low == this.low && r.high == this.high; 
+    		return r.low == this->low && r.high == this->high; 
     	}
     
     	bool disjoint(Range r) {
-    		return r.low > this.high || r.high < this.low; 
+    		return r.low > this->high || r.high < this->low; 
     	}
     
     	bool overlap(Range r) {
-    		return !(this.disjoint(r));
+    		return !(this->disjoint(r));
     	}
     
     	bool lessThan(Range r) {
-    		return this.low < r.low;
+    		return this->low < r.low;
     	}
     
     	bool greaterThan(Range r) {
-    		return this.low > r.low;
+    		return this->low > r.low;
     	}
     
     	bool touching(Range r) {
-    		return r.low == this.low || r.high == this.high;
+    		return r.low == this->low || r.high == this->high;
     	}
     	
     	Range merge(Range r) {
@@ -125,11 +126,11 @@ class Range {
     	Range common(Range r) {
     		if(disjoint(r))
     			return Range();
-    		vector<int> integers{low, high, this.low, this.high};
-    		integers.push_back(low);
-    		integers.push_back(high);
-    		integers.push_back(this.low);
-    		integers.push_back(this.high);
+    		vector<int> integers;
+    		integers.push_back(r.low);
+    		integers.push_back(r.high);
+    		integers.push_back(this->low);
+    		integers.push_back(this->high);
     		sort(integers.begin(), integers.end());
     		return Range(integers[1], integers[2]);
     	}
@@ -138,9 +139,9 @@ class Range {
                 SUBSET, SUPERSET, OVERLAPLEFT, OVERLAPRIGHT, TOUCHINGLEFT, TOUCHINGRIGHT, LEFTDISJOINT, RIGHTDISJOINT, SAME
             };
             Relation classify(Range r) {
-                if (high == r.low) 
+                if (this->high == r.low) 
                     return TOUCHINGRIGHT;
-                if (low == r.high)
+                if (this->low == r.high)
                     return TOUCHINGLEFT;
                 if (equals(r))
                     return SAME;
@@ -149,7 +150,7 @@ class Range {
                 if (r.contains(*this))
                     return SUBSET;
                 if (disjoint(r))
-                    if (low > r.high)
+                    if (this->low > r.high)
                         return RIGHTDISJOINT;
                     else
                         return LEFTDISJOINT;
